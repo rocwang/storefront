a2afdea7d0fee0a90ac361f688770b878f10f291
Roc Wang <roc@kiwiberry.nz>
2016-02-28 23:10:07 +1300

    Remove the injection of CartComponent

--- src/app/components/catalog/catalog.component.ts
+++ src/app/components/catalog/catalog.component.ts
@@ -1,22 +1,24 @@
-import {Component, ChangeDetectorRef, OnInit} from 'angular2/core';
+import {Component, ChangeDetectorRef, OnInit, ViewChild} from 'angular2/core';
 import {Router} from 'angular2/router';
 import {MagentoService} from '../../services/magento.service';
 import {Product} from '../../product';
 import {CartComponent} from '../cart/cart.component';
 
 @Component({
-  selector: 'catalog',
+  selector   : 'catalog',
   templateUrl: 'app/components/catalog/catalog.component.html',
-  directives: [CartComponent],
+  directives : [CartComponent],
 })
 export class CatalogComponent implements OnInit {
   products: Product[];
   title = 'Catalog';
 
+  @ViewChild(CartComponent)
+  private _cart: CartComponent;
+
   constructor(private _router: Router,
               private _magento: MagentoService,
-              private _changeDetectorRef: ChangeDetectorRef,
-              private _cart: CartComponent) {
+              private _changeDetectorRef: ChangeDetectorRef) {
   }
 
   ngOnInit() {
@@ -34,11 +36,11 @@ export class CatalogComponent implements OnInit {
     this._magento.getSwaggerClient().then(api => {
 
       api.catalogProductRepositoryV1.catalogProductRepositoryV1GetListGet({
-        'searchCriteria[filter_groups][0][filters][0][field]': 'visibility',
-        'searchCriteria[filter_groups][0][filters][0][value]': '4',
+        'searchCriteria[filter_groups][0][filters][0][field]'         : 'visibility',
+        'searchCriteria[filter_groups][0][filters][0][value]'         : '4',
         'searchCriteria[filter_groups][0][filters][0][condition_type]': 'like',
-        'searchCriteria[pageSize]': 24,
-        'searchCriteria[currentPage]': 1,
+        'searchCriteria[pageSize]'                                    : 24,
+        'searchCriteria[currentPage]'                                 : 1,
       }).then(data => {
         console.log('Products:', data.obj);
 
