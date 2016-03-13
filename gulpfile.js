bb0b5b5c96b2a2647689a8a27cef69da1a0349b9
Roc Wang <roc@kiwiberry.nz>
2016-03-13 22:03:47 +1300

    Add another ships for IE

--- gulpfile.js
+++ gulpfile.js
@@ -59,8 +59,11 @@ var srcFiles = {
   js      : [
     "node_modules/swagger-client/browser/swagger-client.js",
     "node_modules/jquery/dist/jquery.js",
+
     "node_modules/es6-shim/es6-shim.min.js",
     "node_modules/systemjs/dist/system-polyfills.js",
+    "node_modules/angular2/es6/dev/src/testing/shims_for_IE.js",
+
     "node_modules/angular2/bundles/angular2-polyfills.js",
     "node_modules/systemjs/dist/system.src.js",
     "node_modules/rxjs/bundles/Rx.js",
