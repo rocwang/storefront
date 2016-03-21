c4a8d146c7bb20e76eb1827d825b9243dd9078d3
Roc Wang <roc@kiwiberry.nz>
2016-03-21 13:07:10 +1300

    Formant tweak to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -69,6 +69,7 @@ var srcFiles = {
     "node_modules/systemjs/dist/system.src.js",
     "node_modules/rxjs/bundles/Rx.js",
     "node_modules/angular2/bundles/angular2.dev.js",
+    
     "node_modules/angular2/bundles/router.dev.js",
     "node_modules/angular2/bundles/http.dev.js"
   ],
